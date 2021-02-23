80ed08a68cca22b68451b4ba31a9f2a0f91ee489
eagrundy <ericamalakian@gmail.com>
2021-02-22 22:56:15 -0800

    category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,19 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only: [:show]
+    
+    def index
+        categories = Category.all 
+        render json: categories.to_json(except: [:created_at, :updated_at] ) , include: {category: {only: [:name]} }
+    end
+
+    def show
+        render json: category.to_json(except: [:created_at, :updated_at] )
+    end
+
+    private
+
+    def set_category
+        category = Category.find(params[:id])
+    end
+
 end
