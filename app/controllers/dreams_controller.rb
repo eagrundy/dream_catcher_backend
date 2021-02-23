dae63d14df965b84d2ac8fdc6732043955d887ab
eagrundy <ericamalakian@gmail.com>
2021-02-23 09:25:18 -0800

    Adjustated indentation

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,45 +1,45 @@
 class DreamsController < ApplicationController
-    before_action :set_dream, only: [:show, :update, :destroy]
+  before_action :set_dream, only: [:show, :update, :destroy]
 
-    def index
-        dreams = Category.find_by(id: params[:category_id]).dreams
-        render json: dreams.to_json(except: [:created_at, :updated_at] )
-    end
+  def index
+    dreams = Category.find_by(id: params[:category_id]).dreams
+    render json: dreams.to_json(except: [:created_at, :updated_at] )
+  end
     
-    def show
-        render json: dream.to_json(except: [:created_at, :updated_at])  
-    end
+  def show
+    render json: dream.to_json(except: [:created_at, :updated_at])  
+  end
 
-    def create
-        category = Category.find_by(id: params[:category_id])
-        dream = category.dreams.build(dream_params)
-        if dream.save
-            render json: dream
-        else
-            render json: dream.errors, status: :unprocessable_entity
-        end
+  def create
+    category = Category.find_by(id: params[:category_id])
+    dream = category.dreams.build(dream_params)
+    if dream.save
+      render json: dream
+    else
+      render json: dream.errors, status: :unprocessable_entity
     end
+  end
 
-    def update
-        if dream.update(dream_params)
-          render json: dream
-        else
-          render json: dream.errors, status: :unprocessable_entity
-        end
+  def update
+    if dream.update(dream_params)
+      render json: dream
+    else
+      render json: dream.errors, status: :unprocessable_entity
     end
+  end
     
-      def destroy
-        dream.destroy
-      end
+  def destroy
+     dream.destroy
+  end
 
 
-    private
+  private
 
-    def set_dream
-        dream = Dream.find(params[:id])
-      end
+  def set_dream
+    dream = Dream.find(params[:id])
+  end
 
-    def dream_params
-        params.require(:dream).permit(:name, :description, :image_url, :achieved, :date_achieved, :category_id)
-    end
+  def dream_params
+    params.require(:dream).permit(:name, :description, :image_url, :achieved, :date_achieved, :category_id)
+  end
 end
