a2dce946af1526676fdc8d9b615515e11c8f689d
eagrundy <ericamalakian@gmail.com>
2021-02-27 00:44:02 -0800

    added create to categoriescontroller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,33 @@
 class CategoriesController < ApplicationController
-    before_action :set_category, only: [:show]
+  before_action :set_category, only: [:show, :create]
     
-    def index
-        categories = Category.all 
-        render json: categories.to_json(except: [:created_at, :updated_at] ) , include: {category: {only: [:name]} }
-    end
+  def index
+    categories = Category.all 
+    render json: categories.to_json(except: [:created_at, :updated_at] )
+        #  , include: {category: {only: [:name]} }
+  end
 
-    def show
-        render json: category.to_json(except: [:created_at, :updated_at] )
-    end
+  def show
+    render json: category.to_json(except: [:created_at, :updated_at] )
+  end
 
-    private
-
-    def set_category
-        category = Category.find(params[:id])
+  def create
+    category = Category.create(category_params)
+    if category.save
+      render json: category
+    else
+      render json: category.errors, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def set_category
+    category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 
 end
