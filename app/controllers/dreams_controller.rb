bd20a3946c585aaefd9873b3f551594728682c18
eagrundy <ericamalakian@gmail.com>
2021-04-07 12:20:26 -0700

    Re seeded database

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,38 +1,43 @@
 class DreamsController < ApplicationController
-  before_action :set_dream, only: [:show, :update, :destroy]
+  before_action :set_dream, only: [:show]
 
   def index
+    # dreams = Category.find_by_id(id: params[:category_id]).dreams
     dreams = Dream.all
+    render json: DreamSerializer.new(dreams)
     # dreams = Category.find_by(id: params[:category_id]).dreams
-    render json: dreams.to_json(except: [:created_at, :updated_at] )
+    # render json: dreams, except: [:created_at, :updated_at]
   end
     
   def show
-    render json: dream.to_json(except: [:created_at, :updated_at])  
+    render json: dream.to_json(except: [:created_at, :updated_at], include: {category: {only: [:name]}})  
   end
 
   def create
+    # dream = Dream.create(dream_params)
+    # dream = category.dreams.build(dream_params)
+    dream = Dream.new(dream_params)
     # category = Category.find_by(id: params[:category_id])
-    dream = Dream.create(dream_params)
     # dream = category.dreams.build(dream_params)
     if dream.save
-      render json: dream
+      render json: DreamSerializer.new(dream, include: [:category])
+      # render json: dream
     else
       render json: dream.errors, status: :unprocessable_entity
     end
   end
 
-  def update
-    if dream.update(dream_params)
-      render json: dream
-    else
-      render json: dream.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if dream.update(dream_params)
+  #     render json: dream
+  #   else
+  #     render json: dream.errors, status: :unprocessable_entity
+  #   end
+  # end
     
-  def destroy
-     dream.delete
-  end
+  # def destroy
+  #   dream.delete
+  # end
 
 
   private
@@ -42,6 +47,6 @@ class DreamsController < ApplicationController
   end
 
   def dream_params
-    params.require(:dream).permit(:name, :description, :image_url, :achieved, :date_achieved, :category_id)
+    params.require(:dream).permit(:name, :description, :image_url, :achieved, :date_achieved, :category_id, :id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :dreams
-  resources :categories, only: [:index, :show] do
-    resources :dreams
-  end
+  resources :categories
+  # , only: [:index, :show] do
+  #   resources :dreams, only: [:create, :index]
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
