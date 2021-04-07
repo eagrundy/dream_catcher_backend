557a074d8069d952ac4b44e89bfff3007798e6b4
eagrundy <ericamalakian@gmail.com>
2021-04-07 10:31:51 -0700

    Changed categories controller to include serializer

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,48 +1,12 @@
 class CategoriesController < ApplicationController
-  # before_action :set_category, only: [:show]
     
   def index
     categories = Category.all 
-    render json: categories, only: [:id, :name]
+    options = {include: [:dreams]}
+    # render json: categories, only: [:id, :name]
+    render json: CategorySerializer.new(categories, include: [:dreams])
     # .to_json(except: [:created_at, :updated_at] )
     
   end
-
-  def show
-    category = Category.find_by(id: params[:id])
-    render json: { id: category.id, name: category.name}
-  end
-
-  # def create
-  #   # binding.pry
-  #   category = Category.create(category_params)
-  #   if category.save
-  #     render json: category
-  #   else
-  #     render json: category.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # def update
-  #   if category.update(category_params)
-  #     render json: category
-  #   else
-  #     render json: category.errors, status: :unprocessable_entity
-  #   end  
-  # end
-
-  # def destroy
-  #   category.delete
-  # end
-
-  # private
-
-  # # def set_category
-  # #   category = Category.find(params[:id])
-  # # end
-
-  # def category_params
-  #   params.require(:category).permit(:name)
-  # end
 
 end
