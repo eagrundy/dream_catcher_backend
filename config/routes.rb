e02fbf974f665c0853c0764d6c77da572292e0a2
eagrundy <ericamalakian@gmail.com>
2021-04-06 22:34:14 -0700

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :categories
   resources :dreams
+  resources :categories, only: [:index, :show] do
+    resources :dreams
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
