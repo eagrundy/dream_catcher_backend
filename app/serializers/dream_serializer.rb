85a77c283286ef3a81f0f555afdb2034fe217272
eagrundy <ericamalakian@gmail.com>
2021-04-09 11:20:46 -0700

    Commented out belongs_to in dream serializer

--- app/serializers/dream_serializer.rb
+++ app/serializers/dream_serializer.rb
@@ -1,6 +1,6 @@
 class DreamSerializer
     include FastJsonapi::ObjectSerializer
-    belongs_to :category
+    # belongs_to :category
     attributes :name, :description, :image_url, :achieved, :date_achieved, :category_id
     
   end
