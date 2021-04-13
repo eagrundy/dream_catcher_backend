8ef00f09f81b0490da3462137ff4903712cb0965
eagrundy <ericamalakian@gmail.com>
2021-04-12 22:24:57 -0700

    Updated dreams controller, serializer and model

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,60 +1,54 @@
 class DreamsController < ApplicationController
-  # before_action :set_dream, only: [:show]
 
   def index
-    # dreams = Category.find_by_id(id: params[:category_id]).dreams
     dreams = Dream.all
     render json: DreamSerializer.new(dreams)
-    # dreams = Category.find_by(id: params[:category_id]).dreams
-    # render json: dreams, except: [:created_at, :updated_at]
-  end
-    
-  def show
-    dream = Dream.find(params[:id])
-    render json: dream.to_json(except: [:created_at, :updated_at], include: {category: {only: [:name]}})  
   end
 
   def create
-    # dream = Dream.create(dream_params)
-    # dream = category.dreams.build(dream_params)
     dream = Dream.new(dream_params)
     # byebug
-    dream.category = Category.last
-    # category = Category.find_by(id: params[:category_id])
-    # dream = category.dreams.build(dream_params)
-    if dream.save!
+    if dream.save
       render json: DreamSerializer.new(dream)
       # , include: [:category]
-      # render json: dream
     else
       render json: {error: "Could not save!"}
-    #   render json: dream.errors, status: :unprocessable_entity
+      # render json: dream.errors, status: :unprocessable_entity
     end
   end
 
-  def update
-    dream = Dream.find(params[:id])
-    if dream.update(dream_params)
-        render json: DreamSerializer.new(dream)
-    else
-        render json: {error: "Could not update!"}
-    end
-end
-    
+  
   def destroy
     dream = Dream.find(params[:id])
     dream.destroy
     render json: {message: "Successfully Deleted: #{dream.name}"}
   end
-
-
+  
+  
   private
-
-  # def set_dream
-  #   dream = Dream.find(params[:id])
-  # end
-
+   
   def dream_params
     params.require(:dream).permit(:name, :description, :image_url, :achieved, :date_achieved, :category_id)
   end
 end
+
+
+
+
+# def show
+#   dream = Dream.find(params[:id])
+#   render json: dream.to_json(except: [:created_at, :updated_at], include: {category: {only: [:name]}})  
+# end
+
+# def update
+#   dream = Dream.find(params[:id])
+#   if dream.update(dream_params)
+#       render json: DreamSerializer.new(dream)
+#   else
+#       render json: {error: "Could not update!"}
+#   end
+# end
+
+# def set_dream
+#   dream = Dream.find(params[:id])
+# end
--- app/serializers/dream_serializer.rb
+++ app/serializers/dream_serializer.rb
@@ -1,7 +1,7 @@
 class DreamSerializer
     include FastJsonapi::ObjectSerializer
-    # belongs_to :category
-    attributes :name, :description, :image_url, :achieved, :date_achieved, :category_id
+    belongs_to :category
+    attributes :name, :description, :image_url, :achieved, :date_achieved, :category_id, :category
     
   end
   
